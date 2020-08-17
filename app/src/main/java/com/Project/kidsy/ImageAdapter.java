192b9a1a658ef043931f965febf89bcb59e9082e
Samindi <samindirathnayake99@gmail.com>
2020-08-17 07:05:37 +0530

    My Commit

--- app/src/main/java/com/Project/kidsy/ImageAdapter.java
+++ app/src/main/java/com/Project/kidsy/ImageAdapter.java
@@ -12,7 +12,10 @@ public class ImageAdapter extends BaseAdapter {
 
     private Context mContext;
     public int[] imageArray={
-        R.drawable.story1, R.drawable.story2,R.drawable.story3,R.drawable.story4,R.drawable.story5,R.drawable.story6,R.drawable.story7,R.drawable.story8
+        R.drawable.story1,R.drawable.story17, R.drawable.story2, R.drawable.story10,R.drawable.story11,
+            R.drawable.story3,R.drawable.story13, R.drawable.story4,R.drawable.story5,R.drawable.story6,
+            R.drawable.story18, R.drawable.story14,
+            R.drawable.story15, R.drawable.story8,R.drawable.story16, R.drawable.story12, R.drawable.story9,R.drawable.story7
     };
 
     public ImageAdapter(Context mContext) {
